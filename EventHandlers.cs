4231813475de609046f0e8ac69e0881fe57f73fb
Sisco <111686857+SrSisco@users.noreply.github.com>
2024-07-13 23:53:11 +0200

    v1.2.0 Updated to Exiled 8.9.6

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -24,52 +24,25 @@ namespace WhoIsTKilling
 
         internal void OnHurting(HurtingEventArgs ev)
         {
-            if (ev.Player is null || ev.Attacker is null) return;
-            if (ev.Attacker.Role == RoleTypeId.ClassD && ev.Player.Role == RoleTypeId.ClassD) return;
-            if (ev.Attacker == ev.Player) return;
             attackername = ev.Attacker.Nickname;
             targetname = ev.Player.Nickname;
           
-            
-
+            if (ev.Player is null || ev.Attacker is null) return;
+            if (ev.Attacker.Role == RoleTypeId.ClassD && ev.Player.Role == RoleTypeId.ClassD) return;
+            if (ev.Attacker == ev.Player) return;
 
             if (ev.Attacker.LeadingTeam == ev.Player.LeadingTeam)
             {
 
                 if (plugin.Config.ShowTarget == true)
                 {
-                    var msg = plugin.Config.TargetBc.Replace("{attackername}", ev.Attacker.Nickname);
-                    switch (plugin.Config.NotificationMode)
-                    {
-                        case ("Broadcast"):
-                            ev.Player.Broadcast(plugin.Config.NotificationTime, msg);
-                            break;
-                        case ("Hint"):
-                            ev.Player.ShowHint(msg, plugin.Config.NotificationTime);
-                            break;
-                        case ("Console"):
-                            ev.Player.SendConsoleMessage(msg, "yellow");
-                            break;
-                    }
-                    
+                    ev.Player.Broadcast(plugin.Config.BroadcastTime, plugin.Config.TargetBc);
 
                 }
 
                 if (plugin.Config.ShowShooter == true)
                 {
-                    var msg = plugin.Config.AttackerBc.Replace("{targetname}", ev.Player.Nickname);
-                    switch (plugin.Config.NotificationMode)
-                    {
-                        case ("Broadcast"):
-                            ev.Player.Broadcast(plugin.Config.NotificationTime, msg);
-                            break;
-                        case ("Hint"):
-                            ev.Player.ShowHint(msg, plugin.Config.NotificationTime);
-                            break;
-                        case ("Console"):
-                            ev.Player.SendConsoleMessage(msg, "yellow");
-                            break;
-                    }
+                    ev.Player.Broadcast(plugin.Config.BroadcastTime, plugin.Config.TargetBc);
                 }
 
             }
@@ -82,38 +55,15 @@ namespace WhoIsTKilling
         }
         internal void OnExplodingGrenade(ExplodingGrenadeEventArgs ev)
         {
-            if(ev.Player is null || ev.TargetsToAffect is null) return;
             throwername = ev.Player.Nickname;
             if (!plugin.Config.FlashGrenadeNotify) return;
             if (ev.Projectile.ProjectileType != ProjectileType.Flashbang) return;
-            var msg = plugin.Config.FlashTargetBc.Replace("{throwername}", ev.Player.Nickname);
+
             foreach (Exiled.API.Features.Player player in ev.TargetsToAffect)
             {
-                switch (plugin.Config.NotificationMode)
-                {
-                    case ("Broadcast"):
-                        ev.Player.Broadcast(plugin.Config.NotificationTime, msg);
-                        break;
-                    case ("Hint"):
-                        ev.Player.ShowHint(msg, plugin.Config.NotificationTime);
-                        break;
-                    case ("Console"):
-                        ev.Player.SendConsoleMessage(msg, "yellow");
-                        break;
-                }
+                player.Broadcast(plugin.Config.BroadcastTime, plugin.Config.FlashTargetBc); 
             }
-            switch (plugin.Config.NotificationMode)
-            {
-                case ("Broadcast"):
-                    ev.Player.Broadcast(plugin.Config.NotificationTime, plugin.Config.FlashAttackerBc);
-                    break;
-                case ("Hint"):
-                    ev.Player.ShowHint(plugin.Config.FlashAttackerBc, plugin.Config.NotificationTime);
-                    break;
-                case ("Console"):
-                    ev.Player.SendConsoleMessage(plugin.Config.FlashAttackerBc, "yellow");
-                    break;
-            } 
+            ev.Player.Broadcast(plugin.Config.BroadcastTime, plugin.Config.FlashAttackerBc);  
         }
     }
 }
